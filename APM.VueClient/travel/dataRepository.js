e6fd6fa1704091e4eafbecb804e8eb50da1b35ae
Davor Virovec <dvirovec@gmail.com>
2017-12-26 22:25:54 +0100

    ....

--- APM.VueClient/travel/dataRepository.js
+++ APM.VueClient/travel/dataRepository.js
@@ -12,17 +12,29 @@ var company = {
     "town": { "id": 1, "name": "Zagreb", "country": { "id": 1, "name": "Hrvatska" } }
 };
 
+
+var unit_km_amount = 2.0;
+
 var init_order = {
-    "town_from": 0,
+    "travel_date": '2017-10-01T22:00:00',
     "town_to": 0,
+    "travel_start": Date.now(),
+    "travel_end": Date.now(),
     "relations": [{
         "town_from": { "id": 1, "name": "Zagreb", "country": { "id": 1, "name": "Hrvatska" } },
         "town_to": { "id": 2, "name": "Deutschlandsberg", "country": { "id": 2, "name": "Austria" } },
-        "km": 233
+        "km": 233,
+        "amount":0.0
     },
     {
         "town_from": { "id": 1, "name": "Zagreb", "country": { "id": 1, "name": "Hrvatska" } },
-        "town_to": { "id": 3, "name": "Đurđevac", "country": { "id": 1, "name": "Hrvatska" } }, "km": 120
-    }]
+        "town_to": { "id": 3, "name": "Đurđevac", "country": { "id": 1, "name": "Hrvatska" } },
+        "km": 120,
+        "amount": 0.0
+        }],
+    "costs": [{"description":"","qty":1, "unit_price":0.0, "amount":0.0}],
+    "total_km":0,
+    "total_km_amount": 0.0,
+    "total_costs":0.0
 }; 
 
