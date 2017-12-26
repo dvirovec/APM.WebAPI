6a61d3846202934c8bf3b0ce4fca284a98547574
Davor Virovec <virovec@gmail.com>
2017-12-27 00:02:22 +0100

    ----

---
+++ APM.VueClient/Scripts/component/bs_combo_cost_type.js
@@ -0,0 +1,36 @@
+﻿Vue.component('cost-type-select', {
+    props: {
+        value: {
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        },
+        required: {
+            type: Boolean
+        },
+        readonly: {
+            type: Boolean
+        }
+    },
+
+    template: '<div class="form-inline col-sx-6">' +
+    '<label style="margin-right:.5em;margin-left:.5em;" for="town-select">{{ label }}</label>' +
+    '<select v-bind:value="value" class="form-control" @input="updateValue($event.target.value)">' +
+    '<option v-for="cost_type in cost_type_list" v-bind:value="cost_type.id">{{cost_type.description}}</option>' +
+    '</select></div>',
+    data: {
+        town_list: null,
+        value: null
+    },
+    methods: {
+        updateValue(value) {
+            this.$emit('input', value);
+            this.value = value;
+        }
+    },
+    beforeMount: function () {
+        this.currency_list = currecy_list;
+    }
+});
--- APM.VueClient/travel/dataRepository.js
+++ APM.VueClient/travel/dataRepository.js
@@ -1,4 +1,4 @@
-﻿var currecy = [{ "id": 'EUR', ex_rate: 1 }, { "id": 'EUR', ex_rate: 7.54334 }];
+﻿var currecy_list = [{ "id": 'HRK', ex_rate: 1 }, { "id": 'EUR', ex_rate: 7.54334 }];
 
 var countries = [{ "id": 1, "name": "Hrvatska", "day_amount": 140, "currency":"HRK" },
     { "id": 2, "name": "Austria", "day_amount": 70, "currency": "EUR" }];
@@ -11,6 +11,12 @@ var company = {
     "name": 'INFORBIS', "address": 'Trnjanska cesta 11a',
     "town": { "id": 1, "name": "Zagreb", "country": { "id": 1, "name": "Hrvatska" } }
 };
+
+var cost_type_list = [
+    { "id": 1, "description": 'Vinjeta Slovenija', "unit_price": 15, "currency": { "id": 'EUR', ex_rate: 7.54334 } },
+    { "id": 2, "description": 'Vinjeta Austrija', "unit_price": 8, "currency": { "id": 'EUR', ex_rate: 7.54334 } },
+    { "id": 3, "description": 'Cestarina', "unit_price": 48, "currency": { "id": 'HRK', ex_rate: 1 }  }
+];
 
 
 var unit_km_amount = 2.0;
@@ -32,7 +38,7 @@ var init_order = {
         "km": 120,
         "amount": 0.0
         }],
-    "costs": [{"description":"","qty":1, "unit_price":0.0, "amount":0.0}],
+    "costs": [],
     "total_km":0,
     "total_km_amount": 0.0,
     "total_costs":0.0
