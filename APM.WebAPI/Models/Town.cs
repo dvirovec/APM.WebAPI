504719e2a7eed07e4affefe4dff96c3c92de547e
Davor Virovec <virovec@gmail.com>
2018-01-08 14:39:18 +0100

    Dodao space

--- APM.WebAPI/Models/Town.cs
+++ APM.WebAPI/Models/Town.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }        
         public Country Country { get; set; }
+        public int Citizens { get; set; }
     }
 }
